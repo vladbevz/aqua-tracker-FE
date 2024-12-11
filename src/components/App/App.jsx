01a0b88422db0cd42ac3a45562a50eeb89662e2a
Serhii Tyron <mestn1y@hotmail.com>
2024-12-11 20:27:03 +0200

    add libs and change name components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import { LoginPage } from "../../pages/LoginPage/LoginPage.jsx";
+import { SignInPage } from "../../pages/SignInPage/SignInPage.jsx";
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<h1>HomePage</h1>} />
-      <Route path="/register" element={<h2>RegisterPage</h2>} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/user" element={<h2>UserPage</h2>} />
+      <Route path="/" element={<h1>WelcomePage</h1>} />
+      <Route path="/signup" element={<h2>SignUpPage</h2>} />
+      <Route path="/signin" element={<SignInPage />} />
+      <Route path="/home" element={<h2>HomePage</h2>} />
     </Routes>
   );
 };
