49e86618f03045a5ddfc821b70e094e42a966ff5
Serhii Tyron <mestn1y@hotmail.com>
2024-12-11 02:18:46 +0200

    add some styles and load images

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,30 +1,56 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import css from "./LoginForm.module.css";
 
+const initialValues = {
+  email: "",
+  password: "",
+};
+
 export const LoginForm = () => {
+  const inputSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters")
+      .max(20, "Password must be at most 20 characters")
+      .required("Password is required"),
+  });
+
   return (
     <div className={css.container}>
       <Formik
-        initialValues={{
-          email: "",
-          password: "",
-        }}
+        initialValues={initialValues}
         onSubmit={{}}
+        validationSchema={inputSchema}
       >
         <Form className={css.form} autoComplete="off">
           <label className={css.label}>
-            Email
+            Enter your email
             <Field type="email" name="email" />
+            <ErrorMessage
+              name="email"
+              component="span"
+              className={css.errorMessage}
+            />
           </label>
           <label className={css.label}>
-            Password
+            Enter your password
             <Field type="password" name="password" />
+            <ErrorMessage
+              name="password"
+              component="span"
+              className={css.errorMessage}
+            />
           </label>
           <button className={css.btn} type="submit">
-            Log In
+            Sign In
           </button>
         </Form>
       </Formik>
+      <a>Sign up</a>
     </div>
   );
 };
