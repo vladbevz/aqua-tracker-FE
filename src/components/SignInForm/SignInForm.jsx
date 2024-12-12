42447dbb016c1118ecdfcd5886f37dcde91b8b84
Serhii Tyron <mestn1y@hotmail.com>
2024-12-12 15:50:31 +0200

    add eye btn

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,12 +1,20 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { HiOutlineEyeOff, HiOutlineEye } from "react-icons/hi";
 import css from "./SignInForm.module.css";
+import { useState } from "react";
 const initialValues = {
   email: "",
   password: "",
 };
 
 export const SignInForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   const inputSchema = Yup.object().shape({
     email: Yup.string()
       .email("Invalid email format")
@@ -28,7 +36,12 @@ export const SignInForm = () => {
         <Form className={css.form} autoComplete="off">
           <label className={css.label}>
             Enter your email
-            <Field type="email" name="email" />
+            <Field
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              className={css.field}
+            />
             <ErrorMessage
               name="email"
               component="span"
@@ -37,7 +50,23 @@ export const SignInForm = () => {
           </label>
           <label className={css.label}>
             Enter your password
-            <Field type="password" name="password" />
+            <Field
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="Password"
+              className={css.field}
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className={css.eyeBtn}
+            >
+              {showPassword ? (
+                <HiOutlineEye className={css.pwdIcons} />
+              ) : (
+                <HiOutlineEyeOff className={css.pwdIcons} />
+              )}
+            </button>
             <ErrorMessage
               name="password"
               component="span"
