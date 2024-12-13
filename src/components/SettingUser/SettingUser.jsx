0373cfba158afa27458312890e6d05322d8f90a4
Volodymyr Zahrebelnyi <vov41kttt@gmail.com>
2024-12-14 00:36:18 +0200

    add basic styles

--- src/components/SettingUser/SettingUser.jsx
+++ src/components/SettingUser/SettingUser.jsx
@@ -1,10 +1,24 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { useId } from "react";
+
 import { IoCloseOutline } from "react-icons/io5";
+import { HiOutlineArrowUpTray } from "react-icons/hi2";
+import { HiOutlineEyeOff, HiOutlineEye } from "react-icons/hi";
+
+import css from "./SettingUser.module.css";
+
+const SettingSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Too short!")
+    .max(50, "Too long!")
+    .required("Required"),
+  email: Yup.string().email("Must be a valid email!").required("Required"),
+});
 
 const initialValues = {
   avatar: "",
-  picked: "",
+  picked: "Woman",
   username: "",
   email: "",
   outdatedPassword: "",
@@ -12,88 +26,164 @@ const initialValues = {
   repeatNewPassword: "",
 };
 
-export const SettingUser = ({onCancel}) => {
+export const SettingUser = () => {
   const usernameFieldId = useId();
   const emailFieldId = useId();
-  const outdatedPasswordFielId = useId();
-  const newPasswordFielId = useId();
-  const repeatNewPasswordFielId = useId();
+  const outdatedPasswordFieldId = useId();
+  const newPasswordFieldId = useId();
+  const repeatNewPasswordFieldId = useId();
 
   const handleSubmit = (values, actions) => {
+    console.log(values);
     actions.resetForm();
   };
 
   return (
-    <div>
-      <div>
-        <h1>Setting</h1>
-        <button type="button" onClick={onCancel}>
-          <IoCloseOutline />
+    <div className={css.container}>
+      <div className={css.headerWrap}>
+        <h1 className={css.header}>Setting</h1>
+        <button className={css.closeBtn}>
+          <IoCloseOutline className={css.closeIcon} />
         </button>
       </div>
 
-      <Formik initialValues={{ initialValues }} onSubmit={handleSubmit}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={SettingSchema}
+      >
         <Form>
-          <div>
-            <h2>Your photo</h2>
-            <label htmlFor="avatar">
-              Upload a photo
-              <Field type="file" name="avatar" />
-            </label>
+          <div className={css.photoWrap}>
+            <h2 className={css.title}>Your photo</h2>
+            <div className={css.avatarWrap}>
+              <img
+                className={css.avatar}
+                src="../../../public/images/no avatar/no-avatar.png"
+                alt="avatar"
+              />
+              <HiOutlineArrowUpTray className={css.arrowUpIcon} />
+              <label htmlFor="avatar">
+                Upload a photo
+                <Field type="file" name="avatar" />
+              </label>
+            </div>
           </div>
 
-          <div>
-            <h2>Your gender identity</h2>
-            <label htmlFor="picked">
-              <Field type="radio" name="picked" checked={true} value="Woman" />
-              Woman
-            </label>
-            <label htmlFor="picked">
-              <Field type="radio" name="picked" value="Man" />
-              Man
-            </label>
+          <div className={css.genderWrap}>
+            <h2 className={css.title}>Your gender identity</h2>
+            <div className={css.genderRadioWrap}>
+              <label className={css.genderLabelWrap} htmlFor="picked">
+                <Field type="radio" name="picked" value="Woman" />
+                <p className={css.genderText}>Woman</p>
+              </label>
+              <label className={css.genderLabelWrap} htmlFor="picked">
+                <Field type="radio" name="picked" value="Man" />
+                <p className={css.genderText}>Man</p>
+              </label>
+            </div>
           </div>
 
-          <div>
-            <h2>Your name</h2>
-            <label htmlFor={usernameFieldId}></label>
-            <Field type="text" name="username" id={usernameFieldId} />
-
-            <h2>E-mail</h2>
-            <label htmlFor={emailFieldId}></label>
-            <Field type="email" name="email" id={emailFieldId} />
-          </div>
-
-          <div>
-            <h2>Password</h2>
-            <label htmlFor={outdatedPasswordFielId}>Outdated password:</label>
+          <div className={css.credentialsWrap}>
+            <label className={css.credentialsLabel} htmlFor={usernameFieldId}>
+              Your name
+            </label>
             <Field
-              type="password"
+              className={css.credentialsInput}
+              type="text"
+              name="username"
+              id={usernameFieldId}
+            />
+            <ErrorMessage
+              className={css.errorMessage}
+              name="username"
+              component="span"
+            />
+
+            <label className={css.credentialsLabel} htmlFor={emailFieldId}>
+              E-mail
+            </label>
+            <Field
+              className={css.credentialsInput}
+              type="email"
+              name="email"
+              id={emailFieldId}
+            />
+            <ErrorMessage
+              className={css.errorMessage}
+              name="email"
+              component="span"
+            />
+          </div>
+
+          <div className={css.passwordWrap}>
+            <h2 className={css.title}>Password</h2>
+            <label
+              className={css.passwordLabel}
+              htmlFor={outdatedPasswordFieldId}
+            >
+              Outdated password:
+            </label>
+            <div className={css.passwordInputWrap}>
+              <Field
+                className={css.passwordInput}
+                type="password"
+                name="outdatedPassword"
+                placeholder="Password"
+                id={outdatedPasswordFieldId}
+              />
+              <HiOutlineEyeOff className={css.passwordIcon} />
+            </div>
+            <ErrorMessage
+              className={css.errorMessage}
               name="outdatedPassword"
-              placeholder="Password"
-              id={outdatedPasswordFielId}
+              component="span"
             />
 
-            <label htmlFor={newPasswordFielId}>New password:</label>
-            <Field
-              type="password"
+            <label className={css.passwordLabel} htmlFor={newPasswordFieldId}>
+              New password:
+            </label>
+            <div className={css.passwordInputWrap}>
+              <Field
+                className={css.passwordInput}
+                type="password"
+                name="newPassword"
+                placeholder="Password"
+                id={newPasswordFieldId}
+              />
+              <HiOutlineEyeOff className={css.passwordIcon} />
+            </div>
+            <ErrorMessage
+              className={css.errorMessage}
               name="newPassword"
-              placeholder="Password"
-              id={newPasswordFielId}
+              component="span"
             />
 
-            <label htmlFor={repeatNewPasswordFielId}>
+            <label
+              className={css.passwordLabel}
+              htmlFor={repeatNewPasswordFieldId}
+            >
               Repeat new password:
             </label>
-            <Field
-              type="password"
+            <div className={css.passwordInputWrap}>
+              <Field
+                className={css.passwordInput}
+                type="password"
+                name="repeatNewPassword"
+                placeholder="Password"
+                id={repeatNewPasswordFieldId}
+              />
+              <HiOutlineEyeOff className={css.passwordIcon} />
+            </div>
+            <ErrorMessage
+              className={css.errorMessage}
               name="repeatNewPassword"
-              placeholder="Password"
-              id={repeatNewPasswordFielId}
+              component="span"
             />
           </div>
 
-          <button type="submit">Save</button>
+          <button className={css.saveBtn} type="submit">
+            Save
+          </button>
         </Form>
       </Formik>
     </div>
