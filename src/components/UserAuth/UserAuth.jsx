f998968187d6514167d596fbbcebb99689d06c55
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2024-12-14 19:32:10 +0200

    add small changes
    add small changes

--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -1,23 +1,25 @@
 import css from "./UserAuth.module.css";
 import UserLogo from "../UserLogo/UserLogo";
-// import { useSelector } from "react-redux";
-// import { selectIsLoggedIn } from "../../redux/auth/selectors.js";
-
-const logedIn = true;
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors.js";
+import { NavLink } from "react-router-dom";
+import { RxAvatar } from "react-icons/rx";
+// const logedIn = false;
 
 export const UserAuth = () => {
-  // const logedIn = useSelector(selectIsLoggedIn)
+  const logedIn = useSelector(selectIsLoggedIn)
   return (
     <div className={css.container}>
       {logedIn ? (
         <>
-          {/* <p>User</p>
-          <img src="" alt="avatar" /> */}
         <UserLogo/>
         </>
       ) : (
-        <a href="">Sign In</a>
+       <>
+       <NavLink to='/signin' className={css.link}>Sign In</NavLink>
+       <RxAvatar  className = {css.avatar}/>
+       </> 
       )}
     </div>
-  );
+  )
 };
