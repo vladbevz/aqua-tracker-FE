3fdbaae1795604c94b048686fb7896f960b2d394
Pavlo-Bielohurov <158511567+Pavlo-Bielohurov@users.noreply.github.com>
2024-12-15 01:26:39 +0200

    Add work calculate and dispath(WaterRate )

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,33 +7,35 @@ import css from "./HomePage.module.css";
 import entries from "./entries.json";
 import data from "./data.json";
 
-
 export const HomePage = () => {
-    const [monthData, setMonthData] = useState(data);
-    const [dailyEntries, setDailyEntries] = useState(entries);
-    return (
-        <section className={css.home}>
-            <div className="container">
-                <DailyNorma />
-                <img className="bottle" src="/images/home/mob-home-bottle-1x.png" alt="Bottle" />
-                <WaterRatioPanel />
-                <div className={css.wrapper}>
-                    <TodayWaterList entries={dailyEntries} />
-                    <MonthStatsTable days={monthData} />
-                </div>
-            </div>
-        </section>
-    )
-}; 
-
+  const [monthData, setMonthData] = useState(data);
+  const [dailyEntries, setDailyEntries] = useState(entries);
+  return (
+    <section className={css.home}>
+      <div className="container">
+        <DailyNorma />
+        <img
+          className="bottle"
+          src="/images/home/mob-home-bottle-1x.png"
+          alt="Bottle"
+        />
+        <WaterRatioPanel />
+        <div className={css.wrapper}>
+          <TodayWaterList entries={dailyEntries} />
+          <MonthStatsTable days={monthData} />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 // const addEntry = () => {
-    //         setDailyEntry([...entries, { amount: 200, time: "15:00 PM" }]);
-    // };
+//         setDailyEntry([...entries, { amount: 200, time: "15:00 PM" }]);
+// };
 
-    // const deleteEntry = (id) => {
-    //    const updatedEntries = dailyEntries.filter((entry) => entry.id !== id);
-    // const newTotal = updatedEntries.reduce((sum, entry) => sum + entry.amount, 0);
-    // setWaterEntries(updatedEntries);
-    // setProgress((newTotal / (dailyGoal * 1000)) * 100);
-    // };
+// const deleteEntry = (id) => {
+//    const updatedEntries = dailyEntries.filter((entry) => entry.id !== id);
+// const newTotal = updatedEntries.reduce((sum, entry) => sum + entry.amount, 0);
+// setWaterEntries(updatedEntries);
+// setProgress((newTotal / (dailyGoal * 1000)) * 100);
+// };
