ba67e383c6b42d6c3cb5be3e1ed9b74ee8988e6c
InnaUsenko <innausenko8@gmail.com>
2024-12-16 13:47:45 +0200

    todayWaterList redux

--- src/redux/todayWaterList/operations.js
+++ src/redux/todayWaterList/operations.js
@@ -3,15 +3,57 @@ import axios from "axios";
 
 const URL = "https://aqua-tracker-be.onrender.com";
 
-// POST @ /setwaterrate
-export const setWaterRate = createAsyncThunk(
-  "water/setWaterRate",
-  async (curDaylyNorm, thunkAPI) => {
+// GET @ /water
+export const fetchTodayWater = createAsyncThunk(
+  "todayWater/fetchWater",
+  async (_, thunkAPI) => {
     try {
-      const response = await axios.post(URL + "/setwaterrate", curDaylyNorm);
+      const res = await axios.get(URL + "/water");
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// POST @ /water
+export const addTodayWater = createAsyncThunk(
+  "todayWater/addWater",
+  async (water, thunkAPI) => {
+    try {
+      const response = await axios.post(URL + "/water", water);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+// DELETE @ /water/:id
+export const deleteTodayWater = createAsyncThunk(
+  "todayWater/deleteWater",
+  async (waterId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`${URL}/water/${waterId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+/*
+ * PATCH @ /update
+ * headers: Authorization: Bearer token
+ */
+export const updateTodayWater = createAsyncThunk(
+  "todayWater/updateWater",
+  async (water, thunkAPI) => {
+    try {
+      const res = await axios.post(`${URL}/water/${water.waterId}`);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
