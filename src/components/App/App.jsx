879bbaa5be8901411b5e3fa6218dcf9b845980c7
InnaUsenko <innausenko8@gmail.com>
2024-12-16 10:54:33 +0200

    HotFix change name of persist key

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router";
 import { HomePage } from "../../pages/HomePage/HomePage.jsx";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { SignInPage } from "../../pages/SignInPage/SignInPage.jsx";
 import { SignUpPage } from "../../pages/SignUpPage/SignUpPage.jsx";
 import { WelcomePage } from "../../pages/WelcomePage/WelcomePage.jsx";
@@ -9,7 +9,14 @@ import RestrictedRoute from "../RestrictedRoute/RestrictedRoute";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
 import { SharedLayout } from "../SharedLayout/SharedLayout.jsx";
 import { Toaster } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { refreshUser } from "../../redux/auth/operations.js";
 export const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
     <SharedLayout>
       <Suspense fallback={null}>
