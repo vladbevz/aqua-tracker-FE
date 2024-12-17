1dc3ba3483c70aa398d4a613475330f911e0a3a2
InnaUsenko <innausenko8@gmail.com>
2024-12-17 22:15:26 +0200

    HotFix

--- src/redux/todayWaterList/selectors.js
+++ src/redux/todayWaterList/selectors.js
@@ -1,2 +1,10 @@
 export const selectTodayWaterList = (state) => state.todayWaterList.items;
+export const selectTodayWaterDaylyNorm = (state) =>
+  state.todayWaterList.daylyNorm;
+export const selectTodayWaterAmountWaterPerDay = (state) =>
+  state.todayWaterList.amountWaterPerDay;
+export const selectTodayWaterServings = (state) =>
+  state.todayWaterList.servings;
+export const selectTodayWater = (state) => state.todayWaterList;
 export const selectIsLoading = (state) => state.todayWaterList.isLoading;
+export const selectTodayWaterData = (state) => state.todayWaterList;
