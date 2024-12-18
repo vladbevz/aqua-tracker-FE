6e16a47d58b34552dc0e3f6619f2ecfb4f839441
OleksandrPinchuk <116524115+OleksandrPinchuk@users.noreply.github.com>
2024-12-18 21:24:44 +0100

    home page stryle

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import css from "./DailyNorma.module.css";
 import { DailyNormaModal } from "../DailyNormaModal/DailyNormaModal";
 import { ModalWrap } from "../ModalWrap/ModalWrap.jsx";
@@ -25,7 +25,8 @@ export const DailyNorma = () => {
                 </div>
                 <div className={css.amount}>
                     <p className={css.number}>{dailyNorm} L</p>
-                <button className={css.editBtn} onClick={openModal}>Edit</button>
+                    <button className={css.editBtn} onClick={openModal}>Edit</button>
+                    
                 </div>
             </div>
             {modalIsOpen && <ModalWrap isOpen={modalIsOpen} handleClose={closeModal} >
--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -4,6 +4,7 @@ import { AddWaterModal } from "../AddWaterModal/AddWaterModal";
 import { ModalWrap } from "../ModalWrap/ModalWrap";
 import { useSelector } from "react-redux";
 import { selectTodayWaterPercent } from "../../redux/todayWaterList/selectors";
+import { PiPlusCircleBold } from "react-icons/pi";
 
 export const WaterRatioPanel = () => {
   const [modalIsOpen, setIsOpen] = useState(false);
@@ -18,16 +19,28 @@ export const WaterRatioPanel = () => {
   };
 
   return (
-    <div>
+    <div className={css.box}>
       <div className={css.progressWrapper}>
         <p className={css.textBar}>Today</p>
+        <div className={css.number} style={{ marginLeft: `${progress/1.16}%` }}>{progress}%</div>
         <div className={css.progressBar}>
           <div className={css.progress} style={{ width: `${progress}%` }}></div>
         </div>
+        <div className={css.progressHolder}>
+          <div className={css.spanBox}>
+            <span className={css.span}></span>
+            <p className={css.holder}>0%</p>
+          </div>
+          <div className={css.spanBox}>
+            <span className={css.span}></span>
+            <p className={css.holder}>100%</p>
+          </div>
+        </div>
       </div>
-      <div>
+      <div className={css.btnBox}>
         <button type="button" className={css.addBtn} onClick={openModal}>
-          + Add Water
+          <PiPlusCircleBold className={css.imgBtn} />
+          Add Water
         </button>
       </div>
       {modalIsOpen && (
