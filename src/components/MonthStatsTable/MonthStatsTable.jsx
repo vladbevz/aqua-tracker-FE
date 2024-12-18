4c25e4cf29c440b0636dfc8453369909ff581f2b
Vadim <vsokolvak2000@gmail.com>
2024-12-18 17:49:00 +0200

    add logic changes month, and fetching data to current month

--- src/components/MonthStatsTable/MonthStatsTable.jsx
+++ src/components/MonthStatsTable/MonthStatsTable.jsx
@@ -2,27 +2,63 @@ import css from "./MonthStatsTable.module.css";
 import { HiOutlineChevronLeft } from "react-icons/hi";
 import { HiOutlineChevronRight } from "react-icons/hi";
 
-export const MonthStatsTable = ({ days }) => {
-    return (
-        <div className={css.wrapper}>
-            <div className={css.header}>
-                <h2 className={css.title}>Month</h2>
-                <div className={css.pagination}>
-                    <button className={css.paginationBtn}><HiOutlineChevronLeft /></button>
-                    <p className={css.paginationMonth}>April, 2023</p>
-                    <button className={css.paginationBtn}><HiOutlineChevronRight /></button>
-                </div>
-            </div>
-            <div className={css.calendar}>
-                {days.map((day, index) => (
-                    <div key={index} className={css.item}>
-                        <div key={index} className={`${ css.day } ${day.completed ? `${ css.completed }` : `${ css.uncompleted }`}`}>
-                            <p className={css.date}>{day.date}</p>
-                        </div>
-                        <p className={css.progress}>{day.progress}%</p>
-                    </div>
-                ))}
-            </div>
+export const MonthStatsTable = ({
+  monthData,
+  month,
+  year,
+  setMonthNumber,
+  monthNumber,
+  setYear,
+}) => {
+  const setPrevMonth = () => {
+    if (monthNumber - 1 < 0) {
+      setMonthNumber(11);
+      setYear(year - 1);
+      return;
+    }
+    setMonthNumber(monthNumber - 1);
+  };
+
+  const setNextMonth = () => {
+    if (monthNumber + 1 > 11) {
+      setMonthNumber(0);
+      setYear(year + 1);
+      return;
+    }
+    setMonthNumber(monthNumber + 1);
+  };
+
+  return (
+    <div className={css.wrapper}>
+      <div className={css.header}>
+        <h2 className={css.title}>Month</h2>
+        <div className={css.pagination}>
+          <button className={css.paginationBtn} onClick={setPrevMonth}>
+            <HiOutlineChevronLeft />
+          </button>
+          <p className={css.paginationMonth}>
+            {month}, {year}
+          </p>
+          <button className={css.paginationBtn} onClick={setNextMonth}>
+            <HiOutlineChevronRight />
+          </button>
         </div>
-    )
-}
+      </div>
+      <div className={css.calendar}>
+        {monthData.map((day, index) => (
+          <div key={index} className={css.item}>
+            <div
+              key={index}
+              className={`${css.day} ${
+                day.completed ? `${css.completed}` : `${css.uncompleted}`
+              }`}
+            >
+              <p className={css.date}>{day.date}</p>
+            </div>
+            <p className={css.progress}>{day.progress}%</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
