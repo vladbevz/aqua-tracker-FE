c3667d589168370d7daee59e2d17a2ac18192441
Serhii Tyron <mestn1y@hotmail.com>
2024-12-20 20:59:03 +0200

    change styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, useLocation } from "react-router";
 import { Suspense, useEffect, lazy } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Toaster } from "react-hot-toast";
@@ -20,11 +20,11 @@ const NotFoundPage = lazy(() =>
 export const App = () => {
   const dispatch = useDispatch();
   const isRefreshing = useSelector(selectIsRefreshing);
+
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  //TODO: add some loader when refreshing process is
   return isRefreshing ? (
     <>
       <b>Please wait, updating user info...</b>
