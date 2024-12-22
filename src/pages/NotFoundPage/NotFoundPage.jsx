774185010f1e53e8611533efe0e7d5ff68ce907f
Andriian Siromskyi <157056772+Andriian-Siromskyi@users.noreply.github.com>
2024-12-22 19:45:12 +0200

    not found

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
-
 import css from "./NotFoundPage.module.css";
 
-export default function NotFoundPage () {
+export default function NotFoundPage() {
   return (
     <div className={css.container}>
-      <h1 className={css.title}>The page you requested could not be found.</h1>
+      <h1 className={css.errorCode}>
+        404
+        <span className={css.imageWrapper}>
+          <img
+            src="/public/images/NotFound/notfound-boutle.png"
+            alt="bottle"
+            className={css.bottle}
+          />
+        </span>
+      </h1>
+      <h2 className={css.title}>The page you requested could not be found.</h2>
       <button className={css.btn}>
         <Link to="/">Back to home</Link>
       </button>
     </div>
   );
-};
+}
