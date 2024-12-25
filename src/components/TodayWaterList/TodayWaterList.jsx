6f1bfd16bfe6221c06c40c78fdfa99b04b7a8adc
Serhii Tyron <mestn1y@hotmail.com>
2024-12-26 00:56:51 +0200

    add loader for today water list1

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -1,48 +1,3 @@
-// import { useState } from "react";
-// import { ModalWrap } from "../ModalWrap/ModalWrap.jsx";
-// import { WaterEntry } from "../WaterEntry/WaterEntry";
-// import css from "./TodayWaterList.module.css";
-// import { HiPlusSmall } from "react-icons/hi2";
-// import { AddWaterModal } from "../AddWaterModal/AddWaterModal.jsx";
-// import { useSelector } from "react-redux";
-// import { selectTodayWaterList } from "../../redux/todayWaterList/selectors.js";
-// import { useTranslation } from "react-i18next";
-
-// export const TodayWaterList = () => {
-//   const [modalIsOpen, setIsOpen] = useState(false);
-//   const { t } = useTranslation();
-
-//   const waterList = useSelector(selectTodayWaterList);
-//   const isLoading = useSelector((state) => state.todayWater.isLoading);
-//   const openModal = () => {
-//     setIsOpen(true);
-//   };
-//   const closeModal = () => {
-//     setIsOpen(false);
-//   };
-
-//   return (
-//     <>
-//       <div className="today-section">
-//         <p className={css.text}>{t("homePage.today")}</p>
-//         <ul className={css.list}>
-//           {waterList.map((item) => (
-//             <WaterEntry key={item._id} {...item} />
-//           ))}
-//         </ul>
-//         <button className={css.addBtn} onClick={openModal}>
-//           <HiPlusSmall className={css.plusIcon} />
-//           {t("homePage.addWater")}
-//         </button>
-//       </div>
-//       {modalIsOpen && (
-//         <ModalWrap isOpen={modalIsOpen} handleClose={closeModal}>
-//           <AddWaterModal closeModal={closeModal} />
-//         </ModalWrap>
-//       )}
-//     </>
-//   );
-// };
 import { useState } from "react";
 import { ModalWrap } from "../ModalWrap/ModalWrap.jsx";
 import { WaterEntry } from "../WaterEntry/WaterEntry";
@@ -52,7 +7,7 @@ import { AddWaterModal } from "../AddWaterModal/AddWaterModal.jsx";
 import { useSelector } from "react-redux";
 import { selectTodayWaterList } from "../../redux/todayWaterList/selectors.js";
 import { useTranslation } from "react-i18next";
-import { Puff } from "react-loader-spinner"; // Импортируем лоадер
+import { Puff } from "react-loader-spinner";
 
 export const TodayWaterList = () => {
   const [modalIsOpen, setIsOpen] = useState(false);
