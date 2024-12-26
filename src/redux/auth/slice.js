6326ed54efa2b7705151207823f3b0a1f8f42ea9
Serhii Tyron <mestn1y@hotmail.com>
2024-12-26 13:25:36 +0200

    add loading for app 2

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,61 +1,6 @@
 import { createSlice, isAnyOf } from "@reduxjs/toolkit";
 import { register, logIn, logOut, refreshUser, updateUser } from "./operations";
 
-// const initialState = {
-//   user: {
-//     name: null,
-//     email: null,
-//     avatarUrl: null,
-//     daylyNorm: null,
-//     gender: null,
-//   },
-//   accessToken: null,
-//   isLoggedIn: false,
-//   isRefreshing: false,
-//   error: null,
-// };
-
-// const authSlice = createSlice({
-//   name: "auth",
-//   initialState,
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(register.fulfilled, (state, action) => {
-//         state.user = action.payload.data.user;
-//         state.accessToken = action.payload.data.accessToken;
-//         state.isLoggedIn = true;
-//       })
-//       .addCase(logIn.fulfilled, (state, action) => {
-//         state.user = action.payload.data.user;
-//         state.accessToken = action.payload.data.accessToken;
-//         state.isLoggedIn = true;
-//       })
-//       .addCase(logOut.fulfilled, () => {
-//         return initialState;
-//       })
-//       .addCase(refreshUser.fulfilled, (state, action) => {
-//         state.user = action.payload.data.user;
-//         state.isLoggedIn = true;
-//         state.isRefreshing = false;
-//       })
-//       .addCase(refreshUser.pending, (state) => {
-//         state.isRefreshing = true;
-//       })
-//       .addCase(refreshUser.rejected, (state) => {
-//         state.isRefreshing = false;
-//       })
-//       .addCase(updateUser.fulfilled, (state, action) => {
-//         state.user = action.payload.data.user;
-//       })
-//       .addMatcher(
-//         isAnyOf(register.rejected, logIn.rejected),
-//         (state, action) => {
-//           state.isLoggedIn = false;
-//           state.error = action.payload;
-//         }
-//       );
-//   },
-// });
 const initialState = {
   user: {
     name: null,
@@ -67,7 +12,7 @@ const initialState = {
   accessToken: null,
   isLoggedIn: false,
   isRefreshing: false,
-  isLoading: false, // Добавлено
+  isLoading: false,
   error: null,
 };
 
@@ -76,7 +21,6 @@ const authSlice = createSlice({
   initialState,
   extraReducers: (builder) => {
     builder
-      // Обработка успешных операций
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.data.user;
         state.accessToken = action.payload.data.accessToken;
@@ -110,7 +54,6 @@ const authSlice = createSlice({
         state.user = action.payload.data.user;
         state.error = null;
       })
-      // Общая обработка ошибок
       .addMatcher(
         isAnyOf(register.rejected, logIn.rejected, logOut.rejected),
         (state, action) => {
@@ -118,7 +61,6 @@ const authSlice = createSlice({
           state.error = action.payload;
         }
       )
-      // Общая обработка ожидания
       .addMatcher(
         isAnyOf(register.pending, logIn.pending, logOut.pending),
         (state) => {
